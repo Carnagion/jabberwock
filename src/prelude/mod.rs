0da9c17529add39532424475d832982a2fbbb9f2
Carnagion <carnagion.games@gmail.com>
2022-09-05 09:46:27 +0200

    Refactor to use Hatter's result type alias

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 
 use glob;
 
-use hatter::Error;
+use hatter::Result;
 
 use crate::utils::macros;
 
@@ -15,18 +15,18 @@ pub use config::*;
 pub const INPUT_DIR_VAR: &str = "input";
 pub const OUTPUT_DIR_VAR: &str = "output";
 
-pub fn build(config: &mut Config) -> Result<(), Error>
+pub fn build(config: &mut Config) -> Result<()>
 {
-    let file_rules = self::build_file_rules(config)?;
+    let file_rules = build_file_rules(config)?;
     config.env.push_scope();
     config.env.set(INPUT_DIR_VAR, config.get_input_dir());
     config.env.set(OUTPUT_DIR_VAR, config.get_output_dir());
-    let build_result = self::build_all_files(config, &file_rules);
+    let build_result = build_all_files(config, &file_rules);
     config.env.pop_scope();
     build_result
 }
 
-fn build_file_rules(config: &mut Config) -> Result<HashMap<PathBuf, FileRule>, Error>
+fn build_file_rules(config: &mut Config) -> Result<HashMap<PathBuf, FileRule>>
 {
     let mut file_rules = HashMap::new();
     for (pattern, rule) in config.file_rules()
@@ -41,7 +41,7 @@ fn build_file_rules(config: &mut Config) -> Result<HashMap<PathBuf, FileRule>, E
     Ok(file_rules)
 }
 
-fn build_all_files(config: &mut Config, file_rules: &HashMap<PathBuf, FileRule>) -> Result<(), Error>
+fn build_all_files(config: &mut Config, file_rules: &HashMap<PathBuf, FileRule>) -> Result<()>
 {
     for (in_path, rule) in file_rules.iter().filter(|(path, _)| path.is_file())
     {
@@ -50,17 +50,13 @@ fn build_all_files(config: &mut Config, file_rules: &HashMap<PathBuf, FileRule>)
             FileRule::Ignore => continue,
             FileRule::Copy =>
             {
-                let out_path = Path::new(config.get_output_dir()).join(in_path
-                    .strip_prefix(Path::new(config.get_input_dir()))
-                    .map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?);
+                let out_path = out_path(in_path, config.get_input_dir(), config.get_output_dir())?;
                 fs::create_dir_all(out_path.parent().ok_or_else(|| macros::hatter_error!(RuntimeError, ""))?)?;
                 fs::copy(in_path, out_path)?;
             },
             FileRule::Transpile =>
             {
-                let out_path = Path::new(config.get_output_dir()).join(in_path
-                    .strip_prefix(Path::new(config.get_input_dir()))
-                    .map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?);
+                let out_path = out_path(in_path, config.get_input_dir(), config.get_output_dir())?;
                 let hat = fs::read_to_string(in_path)?;
                 let html = config.env.render(&hat)?;
                 fs::create_dir_all(out_path.parent().ok_or_else(|| macros::hatter_error!(RuntimeError, ""))?)?;
@@ -69,4 +65,12 @@ fn build_all_files(config: &mut Config, file_rules: &HashMap<PathBuf, FileRule>)
         }
     }
     Ok(())
+}
+
+fn out_path(in_path: impl Into<PathBuf>, in_dir: impl Into<PathBuf>, out_dir: impl Into<PathBuf>) -> Result<PathBuf>
+{
+    Ok(out_dir.into()
+        .join(in_path.into()
+            .strip_prefix(in_dir.into())
+            .map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?))
 }
