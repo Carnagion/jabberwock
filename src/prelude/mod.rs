8094dd2e862a9715fd2acccdb7264e6b54ed2b51
Carnagion <carnagion.games@gmail.com>
2022-09-06 10:17:42 +0200

    Refactor site building function

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
-use std::path::PathBuf;
 
 use glob;
 
@@ -59,71 +58,56 @@ pub fn build() -> Result<()>
 /// ```
 pub fn build_with(config: &mut Config) -> Result<()>
 {
-    // Empty output directory
-    fs::remove_dir_all(&config.output_dir)?;
-    fs::create_dir_all(&config.output_dir)?;
-
-    // Construct file rules
-    let file_rules = build_file_rules(config)?;
-
-    // Create new scope in env, build files, and reset scope when done
-    config.env.push_scope();
-    config.env.set(INPUT_DIR_VAR, &config.input_dir);
-    config.env.set(OUTPUT_DIR_VAR, &config.output_dir);
-    let build_result = build_all_files(config, &file_rules);
-    config.env.pop_scope();
-
-    build_result
-}
-
-fn build_file_rules(config: &mut Config) -> Result<HashMap<PathBuf, FileRule>>
-{
     let in_dir_path = Path::new(&config.input_dir);
-    let mut file_rules = HashMap::new();
+    let out_dir_path = Path::new(&config.output_dir);
+
+    fs::remove_dir_all(out_dir_path)?;
+    fs::create_dir_all(out_dir_path)?;
+
+    let mut pattern_rules = HashMap::new();
     for (pattern, rule) in config.file_rules()
     {
         let paths = glob::glob(in_dir_path.join(pattern)
                 .to_str()
-                .ok_or_else(|| macros::hatter_error!(RuntimeError, format!("Path or pattern contains invalid UTF-8 characters: {}, {}", in_dir_path.display(), pattern)))?)
-            .map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?;
+                .ok_or_else(|| macros::hatter_error!(RuntimeError, "Input directory path or glob pattern contains invalid UTF-8 characters"))?)
+            .map_err(|error| macros::hatter_error!(RuntimeError, format!("Invalid glob pattern: {error}")))?;
         for result in paths
         {
-            file_rules.insert(result.map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?, *rule);
+            pattern_rules.insert(result.map_err(|error| macros::hatter_error!(RuntimeError, format!("Inaccessible input file or directory: {error}")))?, *rule);
         }
     }
-    Ok(file_rules)
-}
 
-fn build_all_files(config: &mut Config, file_rules: &HashMap<PathBuf, FileRule>) -> Result<()>
-{
-    for (in_path, rule) in file_rules.iter().filter(|(path, _)| path.is_file())
+    let results = glob::glob(in_dir_path.join("**/*")
+            .to_str()
+            .ok_or_else(|| macros::hatter_error!(RuntimeError, "Input directory path or glob pattern contains invalid UTF-8 characters"))?)
+        .map_err(|error| macros::hatter_error!(RuntimeError, format!("Invalid glob pattern: {error}")))?;
+    for result in results
     {
-        match rule
+        let in_path = result.map_err(|error| macros::hatter_error!(RuntimeError, format!("Inaccessible input file or directory: {error}")))?;
+        if in_path.is_dir()
+        {
+            continue;
+        }
+        let out_path = out_dir_path.join(in_path
+            .strip_prefix(in_dir_path)
+            .map_err(|error| macros::hatter_error!(RuntimeError, format!("Expected input file to be descendant of input directory: {error}")))?);
+        match pattern_rules.get(&in_path).unwrap_or(&FileRule::Ignore)
         {
             FileRule::Ignore => continue,
             FileRule::Copy =>
             {
-                let out_path = out_path(in_path, &config.input_dir, &config.output_dir)?;
                 fs::create_dir_all(out_path.parent().ok_or_else(|| macros::hatter_error!(RuntimeError, format!("Error creating directory: {}", in_path.display())))?)?;
                 fs::copy(in_path, out_path)?;
             },
             FileRule::Transpile =>
             {
-                let out_path = out_path(in_path, &config.input_dir, &config.output_dir)?;
-                let hat = fs::read_to_string(in_path)?;
+                let hat = fs::read_to_string(&in_path)?;
                 let html = config.env.render(&hat)?;
                 fs::create_dir_all(out_path.parent().ok_or_else(|| macros::hatter_error!(RuntimeError, format!("Error creating directory: {}", in_path.display())))?)?;
                 fs::write(out_path.with_extension("html"), html)?;
-            }
+            },
         }
     }
-    Ok(())
-}
 
-fn out_path(in_path: impl Into<PathBuf>, in_dir: impl Into<PathBuf>, out_dir: impl Into<PathBuf>) -> Result<PathBuf>
-{
-    Ok(out_dir.into()
-        .join(in_path.into()
-            .strip_prefix(in_dir.into())
-            .map_err(|error| macros::hatter_error!(RuntimeError, format!("{error}")))?))
+    Ok(())
 }
